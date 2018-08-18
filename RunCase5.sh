66fb06de2855952d1aeaf93a1ddea11be1c3e452
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-08-17 20:06:14 -0500

    Script to run case5

---
+++ RunCase5.sh
@@ -0,0 +1,19 @@
+cd case5N1;
+rm -f log.pbeFoam;
+./Allrun;
+cd ..
+
+cd case5N2;
+rm -f log.pbeFoam;
+./Allrun;
+cd ..
+
+cd case5N3;
+rm -f log.pbeFoam;
+./Allrun;
+cd ..
+
+cd case5N4;
+rm -f log.pbeFoam;
+./Allrun;
+cd ..
