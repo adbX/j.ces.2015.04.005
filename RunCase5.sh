9e2e3898634ef7537a8a366e9dd074606e4d3d28
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-09-14 18:31:29 -0500

    Disable possibly useless cases.

--- RunCase5.sh
+++ RunCase5.sh
@@ -1,19 +1,20 @@
-cd case5N1;
-./Allclean;
-./Allrun;
-cd ..
+#cd case5N1;
+#./Allclean;
+#./Allrun;
+#cd ..
 
-cd case5N2;
-./Allclean;
-./Allrun;
-cd ..
+#cd case5N2;
+#./Allclean;
+#./Allrun;
+#cd ..
 
+# Only experiment necessary?
 cd case5N3;
 ./Allclean;
 ./Allrun;
 cd ..
 
-cd case5N4;
-./Allclean;
-./Allrun;
-cd ..
+#cd case5N4;
+#./Allclean;
+#./Allrun;
+#cd ..
