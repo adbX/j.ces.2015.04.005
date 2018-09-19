5483b0ea4928557c32fc412e67634c37b4328c6c
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-09-19 12:27:06 -0500

    Updating run script to contain all the cases.

--- RunCases.sh
+++ RunCases.sh
@@ -1,20 +1,24 @@
-#cd case5N1;
-#./Allclean;
-#./Allrun;
-#cd ..
-
-#cd case5N2;
-#./Allclean;
-#./Allrun;
-#cd ..
-
-# Only experiment necessary?
 cd case5N3;
 ./Allclean;
 ./Allrun;
 cd ..
 
-#cd case5N4;
-#./Allclean;
-#./Allrun;
-#cd ..
+cd case6N2;
+./Allclean;
+./Allrun;
+cd ..
+
+cd case7N2;
+./Allclean;
+./Allrun;
+cd ..
+
+cd case8N2;
+./Allclean;
+./Allrun;
+cd ..
+
+cd case9N4;
+./Allclean;
+./Allrun;
+cd ..
