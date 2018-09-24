9acf85258f604e03812b6342c80ccbef833f38d6
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-09-24 14:15:17 -0500

    Implemen lognormal inversion algorithm

--- helpers.py
+++ helpers.py
@@ -2,8 +2,10 @@ import math
 import csv
 import subprocess
 import re
+import numpy as np
 import scipy.integrate as integrate
 import scipy.special as special
+import scipy.linalg as linalg
 from shutil import copyfile
 
 def load_probe_data(filepath):
@@ -109,6 +111,16 @@ def extract_analytic_moment(f, xmin, xmax, N):
     # We are explicitly ignoring error here!!
     return integrate.quad(lambda x: (x**N)*f(x), xmin, xmax)[0]
 
+def extract_moments_from_data(x_list, y_list, N):
+    # Extract moments from distribution
+    new_moment = 0.
+    for j in range(0,len(x_list)-1):
+        dx = x_list[j+1]-x_list[j]
+        x = (x_list[j+1]+x_list[j])/2.
+        y = (y_list[j+1]+y_list[j])/2.
+        new_moment += y*(x**N)*dx
+    return new_moment
+
 sigma_re = re.compile("Sigma = (\d*\.\d*)")
 node_re = re.compile("Primary node (\d*)")
 weight_re = re.compile("Primary weight = (\d*\.\d*)")
@@ -165,3 +177,116 @@ def perform_moment_inversion(node_num, analytic_moments, inversion_type='lognorm
         node_definitions[current_node] = temp_info
 
     return node_definitions
+
+def lognormal_eqmom_invert(mom, n, tolerance=1e-15):
+    x = []
+    w = []
+    sig = None
+
+    if len(mom) < 2*n+1:
+        print("Not enough moments provided!")
+        return [x, w, sig]
+
+    if mom[0] < 0:
+        print("negative number density in 1-D quadrature!")
+        return [x, w]
+
+    #1. Guess sigma
+    z = math.exp(1.**2/2.)
+
+    mom_star = []
+    for i in range(0, 2*n+1):
+        mom_star.append(0.)
+
+    print("1")
+    error = np.inf
+    while error > tolerance:
+        print("2")
+        #2. Generate M_star moments
+        for i in range(0, 2*n):
+            mom_star[i] = mom[i]*z**(-(2*i)**2)
+
+        #3. use the wheeler inversion to obtain weights and abscissae for the mom_star moments
+        [chi, weight] = wheeler_invert(mom_star, n)
+        print("wheeler result")
+        print(chi)
+        print(weight)
+
+        x = chi
+        w = weight
+
+        #4. Calculate mom_star[2n]
+        mom_star[2*n] = 0.
+        for i in range(0, n):
+            mom_star[2*n] = weight[i]*chi[i]**(2*n)
+
+        #5. J function and error:
+        error = abs(m[2*n]-z**((2*n)**2)*ms[2*n])
+        if error >= tolerance:
+            z = (mom[2*n]/mom_star[2*n])**(-(2*n)**2)
+
+    sig = math.sqrt(2*math.log(z))
+    return [ w, chi, sig]
+    
+
+def wheeler_invert(mom, n):
+    x = []
+    w = []
+    if len(mom) < 2*n:
+        print("Not enough moments provided!")
+        return [x, w]
+
+    if mom[0] < 0:
+        print("negative number density in 1-D quadrature!")
+        return [x, w]
+    elif mom[0] == 0:
+        w = [0]
+        x = [0]
+        return [x, w]
+    if n == 1:
+        w = mom[0]
+        x = mom[1]/mom[0]
+        nout = 1
+        return [ x, w]
+
+    Z = np.zeros((2*n+1,2*n+1))
+
+    alpha = []
+    beta = []
+
+    # initialize rows
+    for i in range(1,2*n+1):
+        Z[1][i] = mom[i-1] 
+    alpha = [mom[1]/mom[0]]
+    beta = [mom[0]]
+
+    # Calculate recursion coefficients
+    for k in range(2,n+1):
+        # first calculate row values
+        for l in range(k,2*n-k+2):
+            Z[k][l] = Z[k-1][l+1] - alpha[k-2]*Z[k-1][l] - beta[k-2]*Z[k-2][l]
+        # Calc new alpha/beta values
+        alpha.append((Z[k][k+1]/Z[k][k])-(Z[k-1][k]/Z[k-1][k-1]))
+        beta.append(Z[k][k]/Z[k-1][k-1])
+
+    for b in beta:
+        if b < 0:
+            print("Error! Moments not realizable! {}".format(beta))
+            return [x, w]
+
+    diagonal_vals = np.array(alpha)
+    offdiagonal_vals = np.sqrt(np.array(beta[1:]))
+
+    (w_res, v_res) = linalg.eigh_tridiagonal(diagonal_vals, offdiagonal_vals)
+
+    for i in range(0,n):
+        x.append(w_res[i])
+
+    for i in range(0,n):
+        v = v_res[:,i]
+        mag = 0.
+        for j in range(0, n):
+            mag += v[j]*v[j]
+        w.append(((v[0]*mom[0])**2)/mag)
+
+    return [x, w]
