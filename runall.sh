9d044a5b6076775e92466f4fe331d58a85e65f7b
August Shi <awshi2@illinois.edu>
2018-10-31 18:39:36 -0500

    Adding script that runs everything from one place

---
+++ runall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run cases
+bash RunCases.sh
+
+# Run script for tables 2 and 3, figure 1
+python3 Tables2and3andFigure1.py
+
+# Run script for Figures 12 and 13
+python3 Figures12and13.py
+
+# Run script for Figure 14
+python3 Figure14.py
