0e874be0a8633669fd1190539015adb08300db18
August Shi <awshi2@illinois.edu>
2018-11-06 09:35:03 -0600

    Change to using python3, used in Docker

--- Create_Inversion_Plots.sh
+++ Create_Inversion_Plots.sh
@@ -1,7 +1,7 @@
 set -e
 
-python Case_Inversion_Plots.py --case 5 --filename "Figure2.png" --ylim 1.0
-python Case_Inversion_Plots.py --case 6 --filename "Figure4.png" --ylim 0.7
-python Case_Inversion_Plots.py --case 7 --filename "Figure6.png" --ylim 0.7
-python Case_Inversion_Plots.py --case 8 --filename "Figure8.png" --ylim 1.0
-python Case_Inversion_Plots.py --case 9 --filename "Figure10.png" --ylim 0.7
+python3 Case_Inversion_Plots.py --case 5 --filename "Figure2.png" --ylim 1.0
+python3 Case_Inversion_Plots.py --case 6 --filename "Figure4.png" --ylim 0.7
+python3 Case_Inversion_Plots.py --case 7 --filename "Figure6.png" --ylim 0.7
+python3 Case_Inversion_Plots.py --case 8 --filename "Figure8.png" --ylim 1.0
+python3 Case_Inversion_Plots.py --case 9 --filename "Figure10.png" --ylim 0.7
